b43bce28834aec3737f31358abcd0dcd3519e701
Oliver J. <oliver.jaalehto@gmail.com>
2024-01-17 10:49:07 +0200

    vk 1 teht1 koodi

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
-    cout << "Week 1" << endl;
-    return 0;
+    int arvaus;
+
+    std::srand(5);
+    int vastaus = std::rand() % 20;
+
+    //std::cout << "arvottu: " << vastaus << std::endl;   //tarkistetaan toimivuus
+    std::cin >> arvaus;
+
+    while(true) {
+        if (vastaus > arvaus) {
+            std::cout << "lukusi on pienempi kuin oikea vastaus" << std::endl;
+            std::cin >> arvaus;
+        } else if (vastaus < arvaus) {
+            std::cout << "lukusi on suurempi kuin oikea vastaus" << std::endl;
+            std::cin >> arvaus;
+        } else {
+            std::cout << "oikea vastaus" << std::endl;
+            break;
+        }
+    }
+
 }
