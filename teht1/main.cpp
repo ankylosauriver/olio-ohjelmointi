f57f52fcbac3530b5be0929b57ffd30a6559de87
Oliver J. <oliver.jaalehto@gmail.com>
2024-01-24 18:51:54 +0200

    viikon 2 tehtävät

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -2,28 +2,41 @@
 #include <cstdlib>
 
 using namespace std;
+int game(int maxnum);
 
 int main()
 {
+    int kierrokset;
+
+    kierrokset = game(40);
+    cout << "arvattu " << kierrokset << " kertaa." << endl;
+}
+
+int game(int maxnum){
     int arvaus;
+    int veikkaukset;
+
+    veikkaukset = 1;
+
 
     std::srand(5);
-    int vastaus = std::rand() % 20;
+    int vastaus = std::rand() % maxnum;
 
     //std::cout << "arvottu: " << vastaus << std::endl;   //tarkistetaan toimivuus
     std::cin >> arvaus;
-
     while(true) {
         if (vastaus > arvaus) {
             std::cout << "lukusi on pienempi kuin oikea vastaus" << std::endl;
             std::cin >> arvaus;
+            veikkaukset++;
         } else if (vastaus < arvaus) {
             std::cout << "lukusi on suurempi kuin oikea vastaus" << std::endl;
             std::cin >> arvaus;
+            veikkaukset++;
         } else {
             std::cout << "oikea vastaus" << std::endl;
             break;
         }
     }
-
+    return veikkaukset;
 }
